1ebeab05ee26ca85bb1c2ff161e8a3204d17d421
kenny <kenny909@mat.umk.pl>
2024-08-05 00:49:16 +0200

    Improve date format in post

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -2,6 +2,18 @@ const { Messages } = require("../db/queries");
 
 const showMessages = async (req, res) => {
   const messages = await Messages.getAllWithAuthors();
+  messages.forEach(message => {
+    message.added = message.timestamp.toLocaleString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    })
+  })
   res.render('posts', {
     messages,
     user: req.user
