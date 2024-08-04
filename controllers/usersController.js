d43c069b8074a80c7681c464fafe6f45148c2965
kenny <kenny909@mat.umk.pl>
2024-08-04 22:47:13 +0200

    Finish sign-up form

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,12 +4,48 @@ const { User } = require('../db/queries');
 const bcrypt = require('bcryptjs');
 
 const alphaErr = 'must only contain letters'
+const lengthErr = 'must be minimum 1 and maximum 50 length'
 
 // TODO: add validation for each field
 // TODO: check if password is matching
 const validateSignUpForm = [
   body('firstName').trim()
-    .isAlpha().withMessage(`First name ${alphaErr}`),
+    .notEmpty().withMessage('First name cannot be empty')
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 50 }).withMessage(`First name ${lengthErr}`),
+
+  body('lastName').trim()
+    .notEmpty().withMessage('Last name cannot be empty')
+    .isAlpha().withMessage(`Last name name ${alphaErr}`)
+    .isLength({ min: 1, max: 50 }).withMessage(`Last name ${lengthErr}`),
+
+  // TODO: check that the user name is not taken
+  body('userName').trim()
+    .notEmpty().withMessage('username cannot be empty')
+    .isAlpha().withMessage(`username name ${alphaErr}`)
+    // TODO: move general error message instead of hard coding 
+    .isLength({ min: 1, max: 255 }).withMessage(`username must be minimum 1 and maxium 255 length`)
+    .custom(async username => {
+      const existingUser = await User.findByUsername(username);
+      if (existingUser) {
+        throw new Error(`A user with name ${username} alread exists`);
+      }
+    }),
+
+  body('password').trim()
+    .notEmpty().withMessage('Password cannot be empty')
+    .isLength({ min: 8 }).withMessage('Password should be minium 8 characters'),
+
+  body('passwordConfirm')
+    .custom((value, { req }) => {
+      console.log('password: ', req.body.password);
+      console.log('passwordConfirm: ', value);
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match');
+      }
+
+      return true;
+    }),
 ];
 
 const createUserPost = [
@@ -22,11 +58,10 @@ const createUserPost = [
       });
     }
 
-    // TODO: check if already exists user with that username
     const user = req.body;
-    user.password  = await bcrypt.hash(user.password, 10);
+    user.password = await bcrypt.hash(user.password, 10);
     await User.create(user.firstName, user.lastName, user.userName, user.password);
-  
+
     res.redirect('/');
   }),
 ]
--- db/queries.js
+++ db/queries.js
@@ -9,9 +9,20 @@ class User {
     member = 'active',
   ) {
     try {
-      const result = await pool.query(`INSERT INTO users (first_name, last_name, username, password, membership_status) VALUES ($1, $2, $3, $4, $5)`, [firstName, lastName, userName, password, member]);
+      const result = await pool.query('INSERT INTO users (first_name, last_name, username, password, membership_status) VALUES ($1, $2, $3, $4, $5)',
+        [firstName, lastName, userName, password, member]);
     } catch (err) {
       console.log('Error creating user: ', err);
+      throw err;
+    }
+  }
+
+  static async findByUsername(username) {
+    try {
+      const result = await pool.query(`SELECT * FROM users WHERE username = $1`, [username]);
+      return result.rowCount === 0 ? null : result.rows[0];
+    } catch (err) {
+      console.log('Error when searching for user', err);
       throw err;
     }
   }
