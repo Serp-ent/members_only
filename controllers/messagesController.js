fdef01f17b2c7008d3156abb2b9f53e08a642552
kenny <kenny909@mat.umk.pl>
2024-08-05 11:45:01 +0200

    Add validation to creating new messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,5 @@
 const { Messages } = require("../db/queries");
+const { body, validationResult } = require('express-validator');
 
 const showMessages = async (req, res) => {
   const messages = await Messages.getAllWithAuthors();
@@ -14,18 +15,55 @@ const showMessages = async (req, res) => {
       hour12: false
     })
   })
-  
+
   res.render('posts', {
     messages,
     user: req.user
   });
 }
 
+const validateNewMessage = [
+  body('title').trim()
+    .notEmpty().withMessage('Title cannot be empty')
+    .isLength({ min: 1, max: 255 }).withMessage(`Title must be min 1 length and 255 max length`),
+  body('text').trim()
+    .notEmpty().withMessage('Please enter some text')
+    .isLength({ max: 4096 }).withMessage(`Text must be maximum 4096 characters long`),
+];
+
 // TODO: add messages validations 
-const addMessages = async (req, res) => {
-  await Messages.createMessage(req.body.title, req.body.text, req.user.id)
-  res.redirect('/');
-}
+const addMessages = [
+  validateNewMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // TODO: on failure don't clear form
+      // TODO: this should not get new messages from database
+      const messages = await Messages.getAllWithAuthors();
+      messages.forEach(message => {
+        message.added = message.timestamp.toLocaleString('en-US', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false
+        })
+      })
+
+      return res.status(400).render('posts', {
+        messages,
+        user: req.user,
+        errors: errors.array(),
+      });
+    }
+
+    await Messages.createMessage(req.body.title, req.body.text, req.user.id)
+    res.redirect('/');
+  },
+]
 
 module.exports = {
   showMessages,
